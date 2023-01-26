978f0f18ea4916545edbfc676cc1e545fccfc84a
Rojskuu <lerojconstantino@gmail.com>
2023-01-27 02:09:36 +0800

    ok

--- ClassList.cs
+++ ClassList.cs
@@ -170,12 +170,12 @@ namespace AutomatedRoomScheduling
                             " Inner join Section Sec " +
                             " On Sec.SectionID = C.SectionID " +
                             " WHERE ClassID LIKE '%" + FrmDash.SYSem + "%' AND " +
-                            " C.ClassID LIKE '%" + txt.Replace("'", "''") + " AND SectionID LIKE '%" + FrmDash.SYSem + "%' " +
-                            " OR S.SubDescript '%" + txt.Replace("'", "''") + "AND SectionID LIKE '%" + FrmDash.SYSem + "%' " +
-                            " OR T.FName '%" + txt.Replace("'", "''") + "AND SectionID LIKE '%" + FrmDash.SYSem + "%' " + 
-                            " OR T.MName '%" + txt.Replace("'", "''") + "AND SectionID LIKE '%" + FrmDash.SYSem + "%' " + 
-                            " OR T.LName '%" + txt.Replace("'", "''") + "AND SectionID LIKE '%" + FrmDash.SYSem + "%' " + 
-                            " OR Sec.SectionID '%" + txt.Replace("'", "''") + "AND SectionID LIKE '%" + FrmDash.SYSem + "%' " 
+                            " C.ClassID LIKE '%" + txt.Replace("'", "''") + "%' AND SectionID LIKE '%" + FrmDash.SYSem + "%' " +
+                            " OR S.SubDescript '%" + txt.Replace("'", "''") + "%' AND SectionID LIKE '%" + FrmDash.SYSem + "%' " +
+                            " OR T.FName '%" + txt.Replace("'", "''") + "%' AND SectionID LIKE '%" + FrmDash.SYSem + "%' " + 
+                            " OR T.MName '%" + txt.Replace("'", "''") + "%' AND SectionID LIKE '%" + FrmDash.SYSem + "%' " + 
+                            " OR T.LName '%" + txt.Replace("'", "''") + "%' AND SectionID LIKE '%" + FrmDash.SYSem + "%' " + 
+                            " OR Sec.SectionID '%" + txt.Replace("'", "''") + "%' AND SectionID LIKE '%" + FrmDash.SYSem + "%' " 
                         ;
                     adapter = new SqlDataAdapter(query, con);
                     ds = new DataSet();
